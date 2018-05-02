4cf9b3b59a56df194070b2616278ddb7efa1f2ed
parkhi05 <bhatnagarparkhi05@gmail.com>
2018-05-02 12:31:54 +0530

    My First Assignment

---
+++ Assignment1.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment1;
+import java.util.*;
+//Area of a circle
+public class Assignment1 {
+
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        /* float pi=3.14f,result,r;
+       
+        
+        System.out.println("Enter the value of radius : ");
+        r=obj.nextFloat();
+        result=pi*r*r;
+        System.out.println("The area of a circle is : " +result); 
+        ---------------------------------------------------------
+
+
+         */
+         Volume of a cubiod // 
+
+        /* float a,vol;
+        System.out.println("Enter the sides of the cube : ");
+        a=obj.nextFloat();
+        vol=a*a*a;
+        System.out.println("Volume is : "+vol);
+        ---------------------------------------------------------
+
+        */
+
+        Simple Interest 
+
+        /* float p,r,t,SI;
+        System.out.println("Enter the value of P : ");
+        p=obj.nextFloat();
+        System.out.println("Enter the value of R : ");
+          r=obj.nextFloat();
+        System.out.println("Enter the value of T : ");
+          t=obj.nextFloat();
+          SI=p*r*t/100;
+          System.out.println("Simple interest is : "+SI);
+
+        =======================================================================
+*/
+         float l,b,area;
+         System.out.println("Enter the length : ");
+        l=obj.nextFloat();
+         System.out.println("Enter the Breath: ");
+         b=obj.nextFloat();
+         area=l*b;
+         System.out.println("The area of a rectangle is : " +area);
+         }   
+}
