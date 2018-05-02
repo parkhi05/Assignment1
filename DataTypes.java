e8fb34a0a58b61ebcdb559d7c7297eac6c6b3d95
parkhi05 <38903914+parkhi05@users.noreply.github.com>
2018-05-02 12:39:05 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,17 @@
+
+public class DataTypes {
+
+    
+    public static void main(String[] args) {
+        int age=56;
+        char gender='F';
+        String name= "Meena";
+        String uname;
+        System.out.println("The age you are looking for is :" + age );
+        System.out.println("The gender of the person is : " + gender);
+        System.out.println("The name of the person is : " + name);
+        
+      
+    }
+    
+}
---
+++ Sample1.java
@@ -0,0 +1,9 @@
+java.lang.String;
+
+class Sample1
+{ 
+   public static void main(String[] args)
+     {
+        System.out.println("Hello World");
+      }
+}
